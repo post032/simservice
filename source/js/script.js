9a5e2d9cdb0a6bd9a6c91cbc470cb22663642b7a
Elena Ordina <post032@yandex.ru>
2019-04-12 21:40:08 +0300

    слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -37,37 +37,10 @@ $('.modal__close').click(function() {
 });
 
 $('.buyup__choice').slick({
-  dots: true,
+  // dots: true,
   infinite: true,
   speed: 300,
-  slidesToShow: 5,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
 });
